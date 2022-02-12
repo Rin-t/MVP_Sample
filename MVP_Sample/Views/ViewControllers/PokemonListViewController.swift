232039b77060454596ae39ee11c6fc0d912faede
Rin <rin.t.4921@gmail.com>
2022-02-12 22:57:23 +0900

    initial commit

---
+++ MVP_Sample/Views/ViewControllers/PokemonListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonListViewController.swift
+//  MVP_Sample
+//
+//  Created by Rin on 2022/02/12.
+//
+
+import UIKit
+
+class PokemonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
