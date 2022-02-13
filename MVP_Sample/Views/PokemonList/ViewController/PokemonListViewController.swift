36169dccf72820bc7bfd6c7fdffb8f332f96a755
Rin <rin.t.4921@gmail.com>
2022-02-13 18:14:26 +0900

    update PokemonListViewController

--- MVP_Sample/Views/PokemonList/ViewController/PokemonListViewController.swift
+++ MVP_Sample/Views/PokemonList/ViewController/PokemonListViewController.swift
@@ -7,16 +7,49 @@
 
 import UIKit
 
-class PokemonListViewController: UIViewController {
+final class PokemonListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(PokemonListTableViewCell.nib(), forCellReuseIdentifier: PokemonListTableViewCell.identifire)
+        }
+    }
 
-    @IBOutlet weak var tableView: UITableView!
-
+    private var presenter: PokemonListPresenterInput?
+    private var pokemons = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        presenter = PokemonListPresenter(view: self)
+        presenter?.didLoad()
+    }
+}
+
+extension PokemonListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pokemons.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PokemonListTableViewCell.identifire, for: indexPath) as! PokemonListTableViewCell
+        cell.configure(pokemon: pokemons[indexPath.row])
+        return cell
     }
 
 
 }
 
+extension PokemonListViewController: PokemonListPresenterOutput {
+    func showError() {
+        print("error")
+    }
+
+    func showPokemon(_ pokemons: [Pokemon]) {
+        self.pokemons = pokemons
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
