a89cc1153696676e581bb210fd0e825df63c0c0c
Rin <rin.t.4921@gmail.com>
2022-02-13 16:10:37 +0900

    add constants fail

---
+++ MVP_Sample/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MVP_Sample
+//
+//  Created by Rin on 2022/02/13.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let searchPokemonIdRange = 1...151
+}
