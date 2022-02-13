b39d031dc3803179ed4bbe9a558ca265542c0027
Rin <rin.t.4921@gmail.com>
2022-02-13 18:14:10 +0900

    update presenter

--- MVP_Sample/Presenters/PokemonList/PokemonListPresenter.swift
+++ MVP_Sample/Presenters/PokemonList/PokemonListPresenter.swift
@@ -7,4 +7,33 @@
 
 import Foundation
 
+protocol PokemonListPresenterInput: AnyObject {
+    func didLoad()
+}
 
+protocol PokemonListPresenterOutput: AnyObject {
+    func showError()
+    func showPokemon(_ pokemons: [Pokemon])
+}
+
+final class PokemonListPresenter {
+
+    private weak var view: PokemonListPresenterOutput?
+
+    init(view: PokemonListPresenterOutput) {
+        self.view = view
+    }
+}
+
+extension PokemonListPresenter: PokemonListPresenterInput {
+    func didLoad() {
+        Task {
+            do {
+                let pokemons = try await PokemonApiClient.fetchPokemonList()
+                view?.showPokemon(pokemons)
+            } catch {
+                view?.showError()
+            }
+        }
+    }
+}
