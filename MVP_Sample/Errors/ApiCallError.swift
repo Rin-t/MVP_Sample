3fae41884accdadcf7c5976dd03d3e9699df7263
Rin <rin.t.4921@gmail.com>
2022-02-25 21:59:44 +0900

    エラーのリネーム

---
+++ MVP_Sample/Errors/ApiCallError.swift
@@ -0,0 +1,13 @@
+//
+//  ApiCallError.swift
+//  MVP_Sample
+//
+//  Created by Rin on 2022/02/13.
+//
+
+import Foundation
+
+enum ApiCallError: Error {
+    case invalidURL
+    case failToFetchData
+}
