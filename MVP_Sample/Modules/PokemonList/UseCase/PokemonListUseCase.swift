72a3fb65dd23a044a50ade952eabc5ae82d3f9ac
Rin <rin.t.4921@gmail.com>
2022-03-13 22:19:14 +0900

    UseCaseの追加

---
+++ MVP_Sample/Modules/PokemonList/UseCase/PokemonListUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonListUseCase.swift
+//  MVP_Sample
+//
+//  Created by Rin on 2022/03/12.
+//
+
+import Foundation
+
+
+protocol PokemonListUseCaseInput: AnyObject {
+    func featchPokemonList()
+}
+
+final class PokemonLiseUseCase: PokemonListUseCaseInput {
+
+    func featchPokemonList() {
+        
+    }
+}
